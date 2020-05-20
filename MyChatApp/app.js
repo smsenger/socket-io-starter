2838c652bbaf99b44aa747edea1350058a21078b
Saesha Senger <saesha.senger@gmail.com>
2020-05-19 22:39:51 -0400

    commit after merge

---
+++ MyChatApp/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Chat App', 
+    });
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        io.emit('chat message', data)
+    })
+});
+
+// socket.on('chat message', (data) => {
+//     console.log('message:', data)
+//     io.emit('chat message', data)
+// })
+
+http.listen(PORT, () => {
+    console.log(`Chat is running: Listening on http://localhost:${PORT}`)
+});
